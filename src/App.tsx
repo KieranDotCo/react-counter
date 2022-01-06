5b7526691fc205faa7ba7db3176f649464ea92dc
Kieran Whiteman <kieran@kieran.co>
2022-01-06 16:57:55 +0000

    feat: Add Counter logic to <App/>

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, Grid, Typography, IconButton } from "@mui/material";
+import React from "react";
+import "./App.css";
+import RemoveIcon from "@mui/icons-material/Remove";
+import AddIcon from "@mui/icons-material/Add";
 
 function App() {
+  const [counter, setCounter] = React.useState(0);
+
+  React.useEffect(() => {
+    document.addEventListener("keydown", keyDown);
+
+    function keyDown(event: KeyboardEvent) {
+      if (event.key === "ArrowUp") {
+        add();
+      } else if (event.key === "ArrowDown") {
+        minus();
+      }
+    }
+
+    return () => {
+      document.removeEventListener("keydown", keyDown);
+    };
+  }, []);
+
+  function formatNumber(number: number, decimalPlaces = 0): string {
+    return number.toLocaleString(navigator.language, {
+      maximumFractionDigits: decimalPlaces,
+    });
+  }
+
+  function minus() {
+    setCounter((prev) => {
+      return prev - 1;
+    });
+  }
+
+  function add() {
+    setCounter((prev) => {
+      return prev + 1;
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App h100">
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        className="h100"
+      >
+        <Grid item xs={12} className="ta-center">
+          <div>
+            <Typography variant="h1">
+              <IconButton
+                aria-label="minus"
+                onClick={minus}
+                size="large"
+                color="primary"
+              >
+                <RemoveIcon fontSize="inherit" />
+              </IconButton>
+              {formatNumber(counter)}
+              <IconButton
+                aria-label="add"
+                onClick={add}
+                size="large"
+                color="secondary"
+              >
+                <AddIcon fontSize="inherit" />
+              </IconButton>
+            </Typography>
+          </div>
+          <div>
+            <Button
+              variant="outlined"
+              onClick={() => setCounter(0)}
+              size="large"
+            >
+              Reset
+            </Button>
+          </div>
+        </Grid>
+      </Grid>
     </div>
   );
 }
